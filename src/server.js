590d2f55c4b223a7c2af0d57e56c485289980fad
Luis Antonio Canettoli <hello@luisanton.io>
2020-06-26 10:02:03 +0200

    Added server.js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+
+const productsService = require("./services/products")
+const reviewsService = require("./services/reviews")
+
+const errorsHandler = require("./errorsHandler")
+const server = express()
+
+const listEndpoints = require("express-list-endpoints")
+
+
+server.use(express.json())
+server.use(cors())
+
+server.use("/products", productsService)
+server.use("/reviews", reviewsService)
+
+console.log(listEndpoints(server))
+
+server.use(errorsHandler)
+
+server.listen(3001, () => {
+    console.log("server running @ port 3001")
+})
